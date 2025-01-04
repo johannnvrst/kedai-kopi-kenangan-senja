e024fa8bb30b51c1810a84989a216a51f42042d9
Jauhari Rahman <jauharirahman26@gmail.com>
2025-01-04 18:09:57 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbarNav = document.querySelector(".navbar-nav");
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+hamburgerMenu.onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+document.addEventListener("click", function (e) {
+  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
