01a65df8e37fc00875142562e0988186cec82791
Jauhari Rahman <jauharirahman26@gmail.com>
2025-01-05 12:32:36 +0700

    kedai kopi kenangan senja

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const navbarNav = document.querySelector(".navbar-nav");
-const hamburgerMenu = document.querySelector("#hamburger-menu");
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-hamburgerMenu.onclick = () => {
+hm.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
 document.addEventListener("click", function (e) {
-  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
