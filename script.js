77dbaee532a3c5653c8e31394cb7b26c0f378d97
Jauhari Rahman <jauharirahman26@gmail.com>
2025-01-06 23:14:30 +0700

    kedai kopi kenangan senja

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const navbarNav = document.querySelector(".navbar-nav");
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
+const shoppingCart = document.querySelector(".shopping-cart");
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelector(".item-detail-button");
 
 hm.onclick = () => {
   navbarNav.classList.toggle("active");
@@ -16,10 +20,39 @@ document.addEventListener("click", function (e) {
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
   }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
 
 document.querySelector("#search-button").onclick = (e) => {
   searchForm.classList.toggle("active");
   searchBox.focus();
   e.preventDefault();
+};
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
 };
